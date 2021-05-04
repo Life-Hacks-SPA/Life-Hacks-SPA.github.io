30626143b80bba7657e85d8a8d6369d6b3dbe009
Darin Polizanov <72756517+polizanov@users.noreply.github.com>
2021-05-04 16:36:06 +0300

    created home page

--- src/views/home.js
+++ src/views/home.js
@@ -1,3 +1,19 @@
-export function showHome(context){
-    console.log('home');
+import { html } from "../../node_modules/lit-html/lit-html.js"
+
+const home = html`
+<section id="welcome">
+    <div id="welcome-container">
+        <h1>Welcome To Life Hacks World</h1>
+        <p>Share your Hacks, make your life fun, easier...and more!</p>
+        <img src="/images/hack2.jpg" alt="life-hack-image">
+        <h2>Login to see our hacks right away!</h2>
+        <div id="button-div">
+            <a href="#" class="button">Login</a>
+            <a href="#" class="button">Register</a>
+        </div>
+    </div>
+</section>`
+
+export function showHome(context) {
+    context.render(home);
 }
