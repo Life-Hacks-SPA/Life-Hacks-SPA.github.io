725a8a1b5ac235ae748cbb5148806c21951b5a44
Darin Polizanov <72756517+polizanov@users.noreply.github.com>
2021-05-05 14:17:06 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import * as crud from "./api/crud.js";
 window.crud = crud;
-import page from "../node_modules/page/page.mjs";
-import { render } from "../node_modules/lit-html/lit-html.js"
+import page from "./views/imported/importedLibraries.js";
+import { render } from "./views/imported/importedLibraries.js"
 
 import { showAllHacks } from "./views/allHacks.js";
 import { showCreate } from "./views/create.js";
