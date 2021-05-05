6ba42f0d5c89f8d3366338cfa0f30c03cd42806d
Darin Polizanov <72756517+polizanov@users.noreply.github.com>
2021-05-05 16:18:12 +0300

    test

--- src/animations/loadingGif.js
+++ src/animations/loadingGif.js
@@ -3,7 +3,7 @@ import { html } from "../imported/importedLibraries.js";
 const loadingTemplate = html`
 <article id="loading-article">
     <img id="loading-gif" alt="loading" src="/images/secLoading.gif">
-    <p>Loading...</p>
+    <p></p>
 </article>
 `
 
